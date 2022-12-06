1a5adf67b5424fe259c17c9e21921e3cd15677f1
Juste Raimbault <juste.raimbault@ign.fr>
2022-12-06 18:09:05 +0100

    correction_tp_reseaux

--- 3-Reseaux/TP/AS-Reseaux_CORRECTION.R
+++ 3-Reseaux/TP/AS-Reseaux_CORRECTION.R
@@ -30,7 +30,7 @@ E(g)$weight = runif(ecount(g))
 # matrice d'adjacence
 A = as_adj(g)
 B = A%*%A
-sum(as.numeric(B==0))/(n*n)
+sum(as.numeric(B==0))/(n*n) # proportion de couples de sommets relies par un chemin de longueur 2 ou moins
 
 # nombre de composante connexes?
 composantes = components(g)
@@ -64,8 +64,8 @@ diameter(subg)
 
 # diametre pondere
 E(subg)$weight = runif(ecount(subg))
-diameter(subg)
-diameter(subg, weights = E(subg)$weight)
+diameter(subg) # les poids sont pris par default comme l'attribut weight des liens
+diameter(subg, weights = E(subg)$weight) # identique
 
 
 
@@ -109,18 +109,39 @@ V(g)$y = coords[,2]
 plot(g, vertex.size=0, vertex.label=NA)
 
 
-# 1.3) Supprimer des liens aléatoirement;
-#  étudier la taille de la plus grande composante connexe
-#  en fonction du nombre de lien supprimés
-p = 1
+# 1.3) Supprimer des liens aléatoirement dans le graphe en grille
+p = 0.6
 liens_gardes = sample.int(n=ecount(g),size=floor(p*ecount(g)),replace = F)
 gsub = subgraph.edges(g, liens_gardes, delete.vertices = T)
 plot(gsub, vertex.size=0, vertex.label=NA)
 
-# ...
+#  étudier la taille de la plus grande composante connexe
+#  en fonction de la proportion de liens gardés et de la taille du graphe
+
+largest_comp_size <- function(g){
+  comps = components(g)
+  largestcomp = which(comps$csize==max(comps$csize))
+  return(length(which(comps$membership==largestcomp)))
+}
+
+compsizes = data.frame()
+for(n in seq(10,100,10)){
+  show(n)
+  for(p in seq(0.45,0.7,0.01)){
+    for(k in 1:20){ # repetitions alaeatoires
+      g_rep=make_lattice(dimvector = c(n,n))
+      liens_gardes = sample.int(n=ecount(g_rep),size=floor(p*ecount(g_rep)),replace = F)
+      gsubrep = subgraph.edges(g_rep, liens_gardes, delete.vertices = T)
+      compsizes=rbind(compsizes, c(n,p,k,largest_comp_size(gsub)/(n*n)))
+    }
+  }
+}
+colnames(compsizes)<-c("n","p","k","size")
+
+ggplot(compsizes,aes(x=p,y=size,col=n,group=n))+geom_smooth()
 
 
-
+  
 # 1.4) perturber les coordonnées des noeuds de la grille pour obtenir
 #  des plus courts chemins uniques;
 # étudier le diametre en fonction des liens supprimes
@@ -130,11 +151,11 @@ V(gsub)$x = jitter(factor = 1.0,V(gsub)$x)
 V(gsub)$y = jitter(factor = 1.0,V(gsub)$y)
 plot(gsub, vertex.size=0, vertex.label=NA)
 
+# ajouter la distance euclidienne comme poids des liens
 v_ends = ends(gsub,1:ecount(gsub))
 lengths = apply(v_ends, 1, function(e){
   sqrt( (V(gsub)$x[e[1]] - V(gsub)$x[e[2]])^2 + (V(gsub)$y[e[1]] - V(gsub)$y[e[2]])^2 )
 })
-
 E(gsub)$weight = lengths
 
 
@@ -152,11 +173,23 @@ plot(gsub, vertex.size=5, vertex.label=NA,
      )
 
 
-# plus court chemin entre coins dans le reseau en grille
+# plus court chemin entre coins dans le reseau en grille (sur la plus grande composante)
+comps = components(gsub)
+index_of_largest_component = which(comps$csize==max(comps$csize))
+vertices_in_largest = comps$membership==index_of_largest_component
+subgraph_largest = induced_subgraph(gsub, vertices_in_largest)
 
-# ...
+first_col = V(subgraph_largest)[V(subgraph_largest)$x < min(V(subgraph_largest)$x + 1)]
+from = first_col[first_col$y==max(first_col$y)]
 
+last_col = V(subgraph_largest)[V(subgraph_largest)$x > max(V(subgraph_largest)$x - 1)]
+to = last_col[last_col$y==min(last_col$y)]
 
+path = shortest_paths(subgraph_largest,from = from,to = to)$vpath[[1]]
+
+plot(subgraph_largest,vertex.size=5,vertex.label=NA,
+     vertex.color = ifelse(V(subgraph_largest)%in%path,'green','black')
+)
 
 
 
@@ -220,11 +253,21 @@ plot(log(1:length(V(g))), sort(log(strength(g)), decreasing = T))
 # Facultatif : ajuster des power law avec plus de parametres, ou des distributions log-normale
 # package poweRlaw
 
-# ...
+library(poweRlaw)
+wdeg_estimator = poweRlaw::conpl$new(deg_pondere)
+est = poweRlaw::estimate_xmin(wdeg_estimator,xmax = max(deg_pondere))
+wdeg_estimator$setXmin(est)
+
+wdeg_estimator_lnorm = poweRlaw::conlnorm$new(deg_pondere)
+est_lnorm = poweRlaw::estimate_xmin(wdeg_estimator_lnorm,xmax = max(deg_pondere))
+wdeg_estimator_lnorm$setXmin(est_lnorm)
+
+plot(wdeg_estimator);lines(wdeg_estimator, col=2, lwd=2);lines(wdeg_estimator_lnorm, col=3, lwd=2)
 
 
 
-# 1.4) centralites : closeness, betwenness
+
+# 1.4) centralites : closeness, betwenness, eigenvalue
 
 clos = closeness(g)
 clos[clos>quantile(clos,0.99)]
@@ -248,6 +291,7 @@ coms
 
 
 # 1.6) plotter avec multiples infos: communaute, centralite, degre
+# (export dans le fichier "./graph.png")
 png('graph.png',width=20,height = 20, units='cm',res=300)
 plot(
   g,
@@ -271,14 +315,36 @@ dev.off()
 library(osmdata)
 library(sf)
 
+# routes principales pour Paris
 bb <- getbb('paris fr', format_out = 'polygon')
 roads <- opq(bbox = bb, timeout = 200) %>% add_osm_feature(key='highway',value='primary') %>% osmdata_sf()
 ggplot()+geom_sf(data=roads$osm_lines)
-#st_write(...)
+#st_write(...) # pour exporter en shapefile par exemple
 
+# restaurants pour Paris
 restaurants <- opq(bbox = bb, timeout = 200) %>% add_osm_feature(key='amenity',value='restaurant') %>% osmdata_sf()
 ggplot()+geom_sf(data=restaurants$osm_points)
 
-# exporter en sp (donnees non compatibles )
-#roads <- opq(bbox = bb, timeout = 200) %>% add_osm_feature(key='highway',value='primary') %>% osmdata_sp()
+# exporter en sp (pour utilisation avec des packages non compatibles avec sf)
+roads <- opq(bbox = bb, timeout = 200) %>% add_osm_feature(key='highway',value='primary') %>% osmdata_sp()
+
+# transformer les donnees brutes en graphe igraph pour calculer des temps de parcours
+#  -> fonctions disponible ici : https://github.com/JusteRaimbault/TransportationNetwork (pas encore déployé en package)
+source('https://raw.githubusercontent.com/JusteRaimbault/TransportationNetwork/master/NetworkAnalysis/network.R')
+
+# -> les fonctions addTransportationLayer(), addPoints(), addPointsLayer(), addAdministrativeLayer()
+#   permettent de construire itérativement un graphe multimodal
+
+# dans notre cas une seule couche de transport
+#  (pour le snapping = aggregation des noeuds, ici les donnees ne sont pas projetees, on aggrege a 100m ~ 0.001)
+g <- addTransportationLayer(link_layer = roads$osm_lines, snap = 0.001)
+
+# plot d'un plus court chemin aleatoire (vitesse constante = 1 -> a adapter a une vitesse reelle)
+path = shortest_paths(g, from = sample.int(vcount(g),1), to = sample.int(vcount(g),1),weights = 1/E(g)$length)
+plot(g, vertex.size=5, vertex.label=NA,
+     vertex.color = ifelse(V(g)%in%path$vpath[[1]],'green', 'black')
+)
+
+
+
 
